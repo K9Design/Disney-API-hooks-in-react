51a8f9229c672e8aa88db24e39c9a86e42ba942b
K9Design <dog@k9design.co.uk>
2021-11-29 02:09:32 +0000

    added api call to disney and sorted image paths

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,12 @@
 function Character({ character }) {
 
+  // Define a default in case the character doesn't have an image
+  let imageSrc = "https://picsum.photos/300/200/?blur";
+  if (character.imageUrl) {
+    // API seems to include extra path for images so here we strip it off to fetch raw image
+    imageSrc = character.imageUrl.substring(0, character.imageUrl.indexOf('/revision'));
+  }
+
   return (
     <article className="character-item">
 
@@ -9,7 +16,7 @@ function Character({ character }) {
         Add to Favourites
       </div>
 
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
+      <img className="character-item__img" src={imageSrc} alt={character.name} />
 
     </article>
   )
